ae6dec83092e442f64e6927deaef2c51ee53f162
Squirrel <rylancarey2011@gmail.com>
2024-12-04 14:55:27 -0800

    Create diff_prints.go

---
+++ The Go/diff_prints.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Println() can be different than Print().")
+  fmt.Println("Here's an example.")
+  fmt.Println("------------------------------")
+  fmt.Println("Println():")
+  fmt.Println("Greg is very skibidi")
+  fmt.Println("Print():")
+  fmt.Print("I", "am", "skibidi\n")
+  fmt.Println("------------------------------")
+  fmt.Println("Notice the spacing? It happens when you do put commas after each text. You can still use Print like normal tho!")
+  fmt.Println("------------------------------")
+  fmt.Print("greg is very skibidi\n")
+  fmt.Println("that was a Print()!!!")
+
+}
